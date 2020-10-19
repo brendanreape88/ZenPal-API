75f6b053e60355b2f47e98babb02f4d249d991db
Brendan Reape <brendanreape88@gmail.com>
2020-10-19 12:03:34 -0700

    test

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -7,6 +7,7 @@ const jsonBodyParser = express.json();
 authRouter.post("/login", jsonBodyParser, (req, res, next) => {
   const { user_name, user_password } = req.body;
   const loginUser = { user_name, user_password };
+  console.log("REQ BODY", req.body);
 
   for (const [key, value] of Object.entries(loginUser))
     if (value == null)
